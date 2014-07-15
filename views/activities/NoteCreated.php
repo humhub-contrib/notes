c415f79a04e1a0265a25959a0368e9cc1f712026
Lucas Bartholemy <lucas@bartholemy.com>
2014-07-15 13:19:47 +0200

    Fixed missing /strong tag

--- views/activities/NoteCreated.php
+++ views/activities/NoteCreated.php
@@ -1,7 +1,7 @@
 <?php $this->beginContent('application.modules_core.activity.views.activityLayout', array('activity' => $activity)); ?>
 <?php echo Yii::t('SpaceModule.activities', '{userName} created a new note {noteName}.', array(
     '{userName}' => '<strong>'. $user->displayName .'</strong>',
-    '{noteName}' => '<strong>'. $target->getContentTitle()
+    '{noteName}' => '<strong>'. $target->getContentTitle() .'</strong>'
 )); ?>
 <?php $this->endContent(); ?>
 
