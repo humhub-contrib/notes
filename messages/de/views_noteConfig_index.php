3b8c86a005e7bc0572475f7562770730abf1ca6c
Lucas Bartholemy <lucas@bartholemy.com>
2014-11-22 11:39:08 +0100

    Updated translations (translate.humhub.org)

--- messages/de/views_noteConfig_index.php
+++ messages/de/views_noteConfig_index.php
@@ -4,8 +4,8 @@ return array (
   'Back to modules' => 'Zurück zu den Modulen',
   'Could not connect to API!' => 'Konnte nicht mit der API verbinden!',
   'Current Status:' => 'Aktueller Status:',
-  'Notes Module Configuration' => 'Notiz Modul Konfiguration',
-  'Please read the module documentation under /protected/modules/notes/docs/install.txt for more details!' => 'Bitte für weitere Details die Modul Dokumentation unter /protected/modules/notes/docs/install.txt lesen!',
+  'Notes Module Configuration' => 'Konfiguration Notizmodul',
+  'Please read the module documentation under /protected/modules/notes/docs/install.txt for more details!' => 'Bitte für weitere Details die Moduldokumentation unter /protected/modules/notes/docs/install.txt lesen!',
   'Save & Test' => 'Speichern & Testen',
   'The notes module needs a etherpad server up and running!' => 'Das Notizmodul benötigt einen intallierten und laufenden Etherpad Server!',
 );
--- messages/ru/views_notifications_NoteCreated.php
+++ messages/ru/views_notifications_NoteCreated.php
@@ -1,4 +1,4 @@
 <?php
 return array (
-  '{userName} created a new note and assigned you.' => '{userName}  создал новую заметку и назначил на вас',
+  '{userName} created a new note and assigned you.' => '{userName}  создал новую заметку и назначил для вас',
 );
