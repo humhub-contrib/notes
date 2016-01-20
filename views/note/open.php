0385e407ae379bedb7983764ee668f87e9503a4e
Lucas Bartholemy <lucas@bartholemy.com>
2016-01-20 14:43:40 +0100

    Fixed popup html tags

--- views/note/open.php
+++ views/note/open.php
@@ -23,7 +23,7 @@ Assets::register($this);
                              height="40" width="40" alt="40x40" data-src="holder.js/40x40"
                              style="width: 40px; height: 40px; <?php if ($editor['online'] == "false" && $editor['id'] != Yii::$app->user->id) { ?>opacity: 0.5;<?php } ?>"
                              data-toggle="tooltip" data-placement="top" title=""
-                             data-original-title="<strong><?php echo Html::encode($editor['displayName']); ?></strong><br><?php echo Html::encode($editor['title']); ?> <?php if ($editor['online'] == "true" || $editor['id'] == Yii::$app->user->id) { ?>(Online)<?php } ?>">
+                             data-original-title="<?php echo Html::encode($editor['displayName']); ?> <?php if ($editor['online'] == "true" || $editor['id'] == Yii::$app->user->id) { ?>(Online)<?php } ?>">
                     </a>
                     <div class="note-editor-color" style="background: #<?php echo Html::encode($editor['color']); ?>;"></div>
                 </div>
