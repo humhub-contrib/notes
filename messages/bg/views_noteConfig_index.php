9a1b75b78d906142c75d432e6bb5fd49d217ef32
HumHub Translations <info@humhub.com>
2020-11-29 20:46:30 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/bg/views_noteConfig_index.php
+++ messages/bg/views_noteConfig_index.php
@@ -1,12 +1,12 @@
 <?php
 return array (
-  'API Connection successful!' => '',
+  'API Connection successful!' => 'API връзката е успешна!',
   'Back to modules' => 'Върни се при модулите',
-  'Could not connect to API!' => '',
-  'Current Status:' => '',
-  'Notes Module Configuration' => '',
-  'Please read the module documentation under /protected/modules/notes/docs/install.txt for more details!' => '',
-  'Save & Test' => '',
-  'The notes module needs a etherpad server up and running!' => '',
-  'e.g. http://yourdomain/pad/' => '',
+  'Could not connect to API!' => 'Не можах да се свържа с API!',
+  'Current Status:' => 'Актуално състояние:',
+  'Notes Module Configuration' => 'Конфигурация на модула за бележки',
+  'Please read the module documentation under /protected/modules/notes/docs/install.txt for more details!' => 'Моля, прочетете документацията за модула в /protected/modules/notes/docs/install.txt за повече подробности!',
+  'Save & Test' => 'Запази и тествай',
+  'The notes module needs a etherpad server up and running!' => 'Модулът за бележки се нуждае от сървър за etherpad, който е инсталиран и работи!',
+  'e.g. http://yourdomain/pad/' => 'напр. http://yourdomain/pad/',
 );
