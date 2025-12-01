293c2240ffbb8786aa7a0b6634e4e4dbe0926995
Yuriy Bakhtin <yurybakh@gmail.com>
2025-12-01 11:27:20 +0300

    Rename proxy settings

--- libs/EtherpadLiteClient.php
+++ libs/EtherpadLiteClient.php
@@ -405,18 +405,18 @@ class EtherpadLiteClient
      */
     protected function handleCurlProxyOptions(&$curl)
     {
-        if (Yii::$app->settings->get('proxy.enabled')) {
-            curl_setopt($curl, CURLOPT_PROXY, Yii::$app->settings->get('proxy.server'));
-            curl_setopt($curl, CURLOPT_PROXYPORT, Yii::$app->settings->get('proxy.port'));
+        if (Yii::$app->settings->get('proxyEnabled')) {
+            curl_setopt($curl, CURLOPT_PROXY, Yii::$app->settings->get('proxyServer'));
+            curl_setopt($curl, CURLOPT_PROXYPORT, Yii::$app->settings->get('proxyPort'));
 
             if (defined('CURLOPT_PROXYUSERNAME')) {
-                curl_setopt($curl, CURLOPT_PROXYUSERNAME, Yii::$app->settings->get('proxy.user'));
+                curl_setopt($curl, CURLOPT_PROXYUSERNAME, Yii::$app->settings->get('proxyUser'));
             }
             if (defined('CURLOPT_PROXYPASSWORD')) {
-                curl_setopt($curl, CURLOPT_PROXYPASSWORD, Yii::$app->settings->get('proxy.password'));
+                curl_setopt($curl, CURLOPT_PROXYPASSWORD, Yii::$app->settings->get('proxyPassword'));
             }
             if (defined('CURLOPT_NOPROXY')) {
-                curl_setopt($curl, CURLOPT_NOPROXY, Yii::$app->settings->get('proxy.noproxy'));
+                curl_setopt($curl, CURLOPT_NOPROXY, Yii::$app->settings->get('proxyNoproxy'));
             }
         }
     }
